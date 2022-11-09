f28dc36fd121b601769a45846002042ed9b461f4
kimbochen <chentenghung@gmail.com>
2022-11-09 13:54:38 -0800

    Moved clusters array into KMeans function.

--- seq_main.c
+++ seq_main.c
@@ -10,16 +10,11 @@ int main(int argc, char **argv)
 {
     assert(argc == 3);
 
-    int n_pts, n_feats, n_clusters;
+    int n_pts, n_dims, n_clusters;
     float **points, **means;
 
-    readPoints(argv[1], &points, &n_pts, &n_feats);
-    initMeans(argv[2], &means, &n_clusters, points, n_pts, n_feats);
-
-    float **clusters = malloc2DFloatArray(n_clusters, n_feats);
-    for (int i = 0; i < n_clusters * n_feats; i++) {
-        clusters[0][i] = 0.0;
-    }
+    readPoints(argv[1], &points, &n_pts, &n_dims);
+    initMeans(argv[2], &means, &n_clusters, points, n_pts, n_dims);
 
     int *cluster_sizes = (int*) malloc(n_clusters * sizeof(int));
     for (int i = 0; i < n_clusters; i++) {
@@ -31,16 +26,15 @@ int main(int argc, char **argv)
         membership[i] = -1;
     }
 
-    seqKMeans(points, means, clusters, cluster_sizes, membership, n_pts, n_feats, n_clusters);
+    seqKMeans(points, means, cluster_sizes, membership, n_pts, n_dims, n_clusters);
 
 #ifdef PRINT
-    printf("# %d %d\n", n_clusters, n_feats);
-    print2DFloatArray(means, n_clusters, n_feats);
+    printf("# %d %d\n", n_clusters, n_dims);
+    print2DFloatArray(means, n_clusters, n_dims);
 #endif
 
     free2DFloatArray(points);
     free2DFloatArray(means);
-    free2DFloatArray(clusters);
     free(cluster_sizes);
     free(membership);
 }
